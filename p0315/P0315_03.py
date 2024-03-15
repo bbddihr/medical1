57b2819b5274fd56b5b2ce43cafa806921426785
bbddihr <bbddihr@gmail.com>
2024-03-15 18:07:09 +0900

    update_0315_1

--- p0315/P0315_03.py
+++ p0315/P0315_03.py
@@ -1,16 +1,7 @@
 import P0315_02
-
 member = P0315_02.idpw()
 print(member)
 
-# mem.txt 파일에 
-#aaa,1111 저장하시오
-
-f=open("mem.txt","w")
-f.write("aaa,1111\n")
-f.write("bbb,2222\n")
-f.write("ccc,3333\n")
 
 
-f.close()
-print("파일이 저장되었습니다.")
+#
